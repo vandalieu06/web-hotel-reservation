dc89d2feb29e380f3344396a4cd88da52abbfda7
adri <adriamm.97@gmail.com>
2026-01-12 12:02:02 +0100

    Add reservation form and client management features
    - Introduced RoomType constants for room categorization.
    - Enhanced Reserva class to include room number and user email.
    - Created Client class for managing client information.
    - Implemented searchBoxSection function for form validation.
    - Added a new form.html file for the reservation process with Bootstrap styling.
    - Developed styles.css for consistent design across the application.

--- src/js/hotel.js
+++ src/js/hotel.js
@@ -1,3 +1,4 @@
+//CONST rooms
 const RoomType = Object.freeze({
 	SUITE: "suite",
 	DOUBLES: "doubles",
@@ -7,15 +8,46 @@ const RoomType = Object.freeze({
 	PENTHOUSE: "penthouse",
 });
 
-class Reserva{
-    constructor (user, date, roomType){
-        this.user = user;
-        this.date =  date;
-        this.roomType = roomType;
-    }
+//Class
+class Reserva {
+	constructor(email, date, roomType, numRoom) {
+		this.user = email;
+		this.date = date;
+		this.roomType = roomType;
+		this.numRoom = numRoom;
+	}
 
-    getDate(){
-        return this.date;
-    }
+	getDate() {
+		return this.date;
+	}
 }
 
+class Client {
+	constructor(email, telf) {
+		this.email = email;
+		this.telf = telf;
+	}
+
+	getEmail() {
+		return this.email;
+	}
+
+	setTelf(telf) {
+		this.telf = telf;
+	}
+}
+
+//index.html
+function searchBoxSection() {
+	//get values
+	const checkIn = document.getElementById("searchCheckIn").value;
+	const checkOut = document.getElementById("searchCheckOut").value;
+	const guests = document.getElementById("searchGuests").value;
+
+	//validator empty values
+	if (checkIn === "" || checkOut === "") {
+		let message = document.getElementById("error");
+		message.classList.add("color: error")
+	}
+
+}
