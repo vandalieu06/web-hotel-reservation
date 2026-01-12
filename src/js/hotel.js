dbfe089af1bb83202c4b84d5dc6ca5ae927bbe7c
adri <adriamm.97@gmail.com>
2026-01-12 23:08:21 +0100

    Agregar validación y manejo de mensajes en la función searchBoxSection

--- src/js/hotel.js
+++ src/js/hotel.js
@@ -37,19 +37,62 @@ class Client {
 	}
 }
 
+//aux functions
+function typeMessage(messageType, idElement, message) {
+	let span = document.getElementById(idElement);
+
+	switch (messageType) {
+		case "error":
+			span.classList.add("messageError");
+			span.innerText = message;
+			break;
+
+		case "success":
+			span.classList.add("messageSuccess");
+			span.innerText = message;
+			break;
+
+		case "warning":
+			span.classList.add("messageWarning");
+			span.innerText = message;
+			break;
+	}
+}
+
 //index.html
 function searchBoxSection() {
 	//get values
-	const checkIn = document.getElementById("searchCheckIn").value;
-	const checkOut = document.getElementById("searchCheckOut").value;
-
-	
-
-	const guests = document.getElementById("searchGuests").value;
+	let checkIn = document.getElementById("searchCheckIn").value;
+	let checkOut = document.getElementById("searchCheckOut").value;
+	let guests = document.getElementById("searchGuests").value;
 
 	//validator empty values
 	if (checkIn === "" || checkOut === "") {
-		let message = document.getElementById("error");
-		message.classList.add("color: error");
+		typeMessage("error", "error", "Porfavor, seleccione una fecha");
+		return;
 	}
+
+	//Date transform
+	checkInDate = new Date(checkIn);
+	checkOutDate = new Date(checkOut);
+
+	//size validator
+	if (checkInDate > checkOutDate) {
+		typeMessage(
+			"error",
+			"error",
+			"No puede ser el checkIn después del checkOut"
+		);
+	}
+
+	//create semi-object
+	const datosIniciales = {
+		checkIn,
+		checkOut,
+		guests: Number(guests),
+	};
+
+	localStorage.setItem("DatosIniciales", JSON.stringify(datosIniciales));
 }
+
+// form.html
